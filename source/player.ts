053d110a287bf8c616fc3193c8bcb08bd6f27758
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2017-03-28 12:55:13 +1030

    Add the ability for the player to jump

--- source/player.ts
+++ source/player.ts
@@ -1,4 +1,5 @@
 import { GameObject } from "../../engine/game_object";
+import { Input } from "../../engine/input";
 import { Sprite } from "../../engine/sprite";
 
 import { Section } from "./section";
@@ -6,11 +7,21 @@ import { Type } from "./section";
 
 
 export class Player extends GameObject {
+  private readonly keycodeSpace: number = 32;
   private speed: number;
+  private shadowSprite: Sprite
   private downSprite: Sprite;
   private angleSprite: Sprite;
   private section: Section;
   private sectionStartTime: number = 0;
+
+  private readonly initialJumpVelocity: number = -500;
+  private gravity: number = 1400;
+  private jumpPosition: number = 0;
+  private jumpVelocity: number = 0;
+  private jumping: boolean = false;
+  private doubleJumping: boolean = false;
+
 
   constructor(speed: number) {
     super();
@@ -18,6 +29,8 @@ export class Player extends GameObject {
         this.addSpriteFromFile("app/game/resources/images/player_down.png");
     this.angleSprite =
         this.addSpriteFromFile("app/game/resources/images/player_angle.png");
+    this.shadowSprite =
+        this.addSpriteFromFile("app/game/resources/images/shadow.png");
     this.speed = speed;
   }
 
@@ -27,19 +40,6 @@ export class Player extends GameObject {
   }
 
   update(deltaSeconds: number) {
-    // Updates sprite
-    switch (this.section.type) {
-      case Type.Down:
-        this.currentSprite = this.downSprite;
-        break;
-      case Type.Left:
-        this.currentSprite = this.angleSprite;
-        break;
-      case Type.Right:
-        this.currentSprite = this.angleSprite;
-        break;
-    }
-    
     // Catches the player up to the current section due to any lag
     let secsSinceStartOfSection = (Date.now() - this.sectionStartTime) / 1000;
     let secsPerSection = Section.pathVerticalLength / this.speed;
@@ -60,5 +60,55 @@ export class Player extends GameObject {
     let pathHorizontalLength = this.section.nextJoinX - this.section.prevJoinX;
     let startPositionX = this.section.x + this.section.prevJoinX;
     this.x = startPositionX + t * pathHorizontalLength;
+
+    this.playerInput(deltaSeconds);
+  }
+
+  private playerInput(deltaSeconds: number): void {
+    if (Input.keysDown[this.keycodeSpace] || Input.clicked) {
+      this.jump();
+    }
+
+    if (this.jumping) {
+      this.jumpPosition += this.jumpVelocity * deltaSeconds;
+      this.jumpVelocity += this.gravity * deltaSeconds;
+    }
+
+    if (this.jumpPosition >= 0) {
+      this.jumping = false;
+      this.doubleJumping = false;
+      this.jumpPosition = 0;
+    }
+  }
+
+  private jump(): void {
+    if (!this.jumping) {
+      this.jumping = true;
+      this.jumpVelocity = this.initialJumpVelocity;
+    } else if (!this.doubleJumping) {
+      this.jumpVelocity = this.initialJumpVelocity;
+      this.doubleJumping = true;
+    }
+  }
+
+  private updateSprite(): void {
+    switch (this.section.type) {
+      case Type.Down:
+        this.currentSprite = this.downSprite;
+        break;
+      case Type.Left:
+        this.currentSprite = this.angleSprite;
+        break;
+      case Type.Right:
+        this.currentSprite = this.angleSprite;
+        break;
+    }
+
+  }
+
+  draw(context) {
+    this.updateSprite();
+    this.shadowSprite.draw(context, this.x, this.y);
+    this.currentSprite.draw(context, this.x, this.y + this.jumpPosition);
   }
 }
